058a3b1fcdf3965de7da5b3a05c4579070e4605e
DAVIDLUO <david.luo@dbschenker.com>
2020-01-08 09:20:13 -0500

    adding starting project

---
+++ style.css
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.kk</p>
+
+<script type="text/javascript" src="test.js"></script>
+<script type="text/javascript"></script>
+</body>
+</html>
