e93f91286d81a91c9d2018a816cbe571be5cc8fb
DAVIDLUO <david.luo@dbschenker.com>
2020-01-08 16:21:26 -0500

    change all files

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-<h1>This is a Heading</h1>
-<p>This is a paragraph.kk</p>
-
-<script type="text/javascript" src="test.js"></script>
-<script type="text/javascript"></script>
-</body>
-</html>
+function myFunction() {
+ document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
